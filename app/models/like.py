3f0ca399bd1c496d05a8c78fa87fac5b4b47b080
Joseph <poedude5229@gmail.com>
2024-08-03 22:34:51 -0400

    Default Text

---
+++ app/models/like.py
@@ -0,0 +1,7 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+class Like(db.Model):
+    __tablename__ = 'likes'
+
+    if environment == "production":
+        __table_args__ = {"schema": SCHEMA}
