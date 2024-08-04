cb103d612702b1525d1e238a7b3ad0718200951b
Joseph <poedude5229@gmail.com>
2024-08-04 02:44:05 -0400

    Added model for Blog

--- app/models/blog.py
+++ app/models/blog.py
@@ -5,3 +5,18 @@ class Blog(db.Model):
 
     if environment == "production":
         __table_args__ = {"schema": SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    blog_icon_url = db.Column(db.String(255), nullable=False)
+    blog_banner_url = db.Column(db.String(255))
+    genre = db.Column(db.String(100))
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'blog_icon_url': self.blog_icon_url,
+            'blog_banner_url': self.blog_banner_url,
+            'genre': self.genre
+        }
