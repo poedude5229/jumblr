e7cc94f1e83a8b464604736a1cad941431de7002
Joseph <poedude5229@gmail.com>
2024-08-09 13:03:21 -0400

    Post table done

--- app/models/post.py
+++ app/models/post.py
@@ -7,4 +7,16 @@ class Post(db.Model):
         __table_args__ = {"schema": SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    
+    user_id = db.Column(db.Integer)
+    blog_id = db.Column(db.Integer)
+    source = db.Column(db.String(255))
+    type = db.Column(db.String(50))
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'blog_id': self.blog_id,
+            'source': self.source,
+            'type': self.type
+        }
