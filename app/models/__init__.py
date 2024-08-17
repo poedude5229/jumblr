64f94c52c8505ab505059466069ae45a409fc306
Joseph <poedude5229@gmail.com>
2024-08-17 11:48:19 -0400

    DB configured

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,8 @@
 from .db import db
 from .user import User
+from .blog import Blog
+from .post import Post
+from .jote import Jote
+from .like import Like
+from .follow import Follow
 from .db import environment, SCHEMA
