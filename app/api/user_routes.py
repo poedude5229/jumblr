ac684fa6e3f438121803c05aef462d974db6d5ef
Joseph <poedude5229@gmail.com>
2024-09-14 19:24:09 -0400

    Corrected user id API route for some needs

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required
-from app.models import User
+from app.models import User, Blog, Post, Jote, Follow, Like
 
 user_routes = Blueprint('users', __name__)
 
@@ -22,4 +22,13 @@ def user(id):
     Query for a user by id and returns that user in a dictionary
     """
     user = User.query.get(id)
-    return user.to_dict()
+    userdict = user.to_dict()
+    blogs = Blog.query.filter(Blog.user_id == id).all()
+    posts = Blog.query.filter(Post.user_id == id).all()
+    userdict["blogs"] = []
+    for blog in blogs:
+        userdict['blogs'].append(blog.to_dict())
+    userdict['posts'] = []
+    for post in posts:
+        userdict['posts'].append(post.to_dict())
+    return jsonify(userdict)
