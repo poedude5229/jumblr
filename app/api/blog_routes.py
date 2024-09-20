3519afb3e87a40ad08311db9091f52dc4f0e6550
Joseph <poedude5229@gmail.com>
2024-09-20 14:16:44 -0400

    Blog by id route

--- app/api/blog_routes.py
+++ app/api/blog_routes.py
@@ -9,7 +9,17 @@ def blogs():
     blogs = Blog.query.all()
     return {'blogs': [blog.to_dict() for blog in blogs]}
 
+# @blog_routes.route('/user_blogs/<user>')
+# def blogs_by_user(user):
+#     specified_blogowner = User.query.filter(User.username == user).all()
+#     blogs3 = Blog.query.filter(Blog.user_id == specified_blogowner.id).all()
+#     return {'user_blogs': [blog.to_dict() for blog in blogs3]}
 @blog_routes.route('/<type>')
 def blogs_by_type(type):
-    blogs2 = Blog.query.filter(Blog.genre == type)
+    blogs2 = Blog.query.filter(Blog.genre == type).all()
     return {'blogs': [blog.to_dict() for blog in blogs2]}
+
+@blog_routes.route('/<int:id>')
+def blog_by_id(id):
+    blog3 = Blog.query.get(id)
+    return {'blog': blog3.to_dict()}
